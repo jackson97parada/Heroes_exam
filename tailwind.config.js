8d03182beb9c6bfe8684adce83fe733adba3cb11
jackson Arevalo <jackson97parada@gmail.com>
2022-12-12 11:39:24 -0500

    update

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    fontFamily: {
+      DM: ["DM Sans", "sans-serif"],
+      Poppin: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        blueDark: "#2F1893",
+        blueBlack: "#1E0E62",
+      },
+      backgroundImage: {
+        bgBird: "url('../images/bird.png')",
+        bgSweet: "url('../images/sweet.png')",
+        bgLadders: "url('../images/background.png')",
+        bgSweet2: "url('../images/background-sweet.png')",
+      },
+    },
+  },
+  safelist: [
+    "w-64",
+    "w-1/2",
+    "rounded-l-lg",
+    "rounded-r-lg",
+    "bg-gray-200",
+    "grid-cols-4",
+    "grid-cols-7",
+    "h-6",
+    "leading-6",
+    "h-9",
+    "leading-9",
+    "shadow-lg",
+  ],
+  plugins: [
+    // include Flowbite as a plugin in your Tailwind CSS project
+    require("flowbite/plugin"),
+    require("@tailwindcss/forms"),
+  ],
+};
